9ab1f18e41904b605114ba91ad31094223fe66c6
NachoPena <nachokillah0gmail.com>
2023-03-15 20:38:11 -0300

    Se sube el ejercicio 3

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int resultadoSuma = sumaTresNumeros(3, 7, 2);
+        System.out.println("El resultado de la suma es: " + resultadoSuma);
+    }
+    public static int sumaTresNumeros(int num1, int num2, int num3) {
+        int resultado = num1 + num2 + num3;
+        return resultado;
+    }
+
+}
+
+
